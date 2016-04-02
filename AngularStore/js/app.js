462b49691b65df02613101d5ad09478d2af2e5db
Shehtab Zaman <shehtabzaman@Shehtabs-MacBook-Air.local>
2016-04-01 23:16:16 -0400

    Framework store added tabs

--- AngularStore/js/app.js
+++ AngularStore/js/app.js
@@ -1,30 +1,31 @@
 (function(){
 var app = angular.module('store',[]);
-var gems = [{
-  name: "Python",
-  version: 3.5,
-  price: 2.95,
-  description: '...',
-  canPurchase: true,
-  soldOut: true
-},{
-  name: "Ruby",
-  version:,
-  price: 5.95,
-  description: "...",
-  canPurchase: true
-},
-{
-  name: "PHP",
-  version: ,
-  price: 6,
-  description: "...",
-  canPurchase: true
-}];
 
-app.controller('StoreController',function(){
-  this.products = gems;
+app.controller('StoreController',['$http',function($http){
+  var store =this;
+  store.products = [];
+  $http.get('info.json').success(function(data){
+    store.products = data;
+  });
+}]);
+
+app.controller('ReviewController',function(){
+  this.review = {};
+  this.addReview = function(product){
+    product.reviews.push(this.review);
+    this.review = {};
+  };
 });
 
+app.controller('TabController',function(){
+  this.tab = 0;
+  this.setTab = function(num){
+    this.tab = num;
+  };
+
+  this.isSet = function(num){
+    return this.tab === num;
+  };
+});
 
 })();
