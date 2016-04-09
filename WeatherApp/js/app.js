988a95cb53783e5e43be7ec7ad6672907be81f99
Shehtab Zaman <shehtabzaman@gmail.com>
2016-04-09 00:23:20 -0400

    button click animation

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -1,59 +1,30 @@
-(function(){
-var app = angular.module('food',[]);
 
-app.controller('SearchController',['$http',function($http){
-  var search = this;
-  search.results = {};
-  this.submit = function(){
-    $http.get('images.json').success(function(data){
-      // console.log(data[0].results['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfjFNW0_-kdinku_amiWbT4_8VvAiroSpmpr4PCiDJDR2vDwg3H_eJpGHD']);
-      for (var key in data[0].results){
-        //console.log(data[0].results[key]);
-        search.results[key] = data[0].results[key];
-        //console.log(search.results);
-      }
-      //search.results = data[0];
-      console.log(search.results);
-    });
-    //console.log(search.results);
-  }
+function kelvinToCel(temp){
+  return (temp - 273);
+}
 
-  }
-]);
+function kelvinToFahrenheit(temp){
+  var cels = kelvinToCel(temp);
+  return (cels*(1.8)) + 32;
+}
+$(document).ready(function(){
+  $('#toggleButton').click(function(){
+    var tempString = $('#kelvin').text();
+    // console.log(tempString);
+    var tempInt = parseInt(tempString);
+    // console.log(tempInt);
+    $('#Celsius').text(kelvinToCel(tempInt));
+    $('#Fahrenheit').text(kelvinToFahrenheit(tempInt));
+    if($('#Fahrenheit').css('display') == 'none'){
+      $('#Celsius').fadeToggle(1000,"swing");
+      $('#Fahrenheit').delay(1000).fadeToggle(1000,"swing");
 
-app.controller('formCtrl',['$http','$scope',function($http,$scope){
-  this.query = '';
-  // this.submit = function(){
-  //   this.queryText = queryText;
-  // };
-  this.submit = function(){
-    $http.get('images.json').success(function(data){
-      console.log(data[0].results);
-    });
-    console.log(this.query);
-  }
-  this.returnQuery = function(){
-    return this.queryText;
-  }
-}]);
+    }
+    if($('#Celsius').css('display')=='none'){
+      $('#Fahrenheit').fadeToggle(1000,"swing");
+      $('#Celsius').delay(1000).fadeToggle(1000,"swing");
+    }
 
-app.controller('imageCtrl',['$http',function($http){
-  this.imageUrls = [];
-  this.addImage=function(imageUrl){
-    console.log(imageUrl);
-    this.imageUrls.push(imageUrl);
-  };
 
-  this.sendImages = function(){
-    console.log(this.imageUrls);
-    $http({
-      url:'http://127.0.0.1:5000/imageTags',
-      method: 'GET',
-      params: {'urls':JSON.stringify(this.imageUrls)}
-    }).success(function(data){
-      console.log(data);
-    });
-  };
-
-}]);
-})();
+  });
+});
