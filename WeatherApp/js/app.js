2890c6dbc1f6f6b2c81222f05fc907edcfefd04a
Shehtab Zaman <shehtabzaman@gmail.com>
2016-04-09 01:38:45 -0400

    New Stylesheet, style.css, fixed tabs. Default tabs added for initial page load

--- WeatherApp/js/app.js
+++ WeatherApp/js/app.js
@@ -2,7 +2,6 @@
 function kelvinToCel(temp){
   return (temp - 273);
 }
-
 function kelvinToFahrenheit(temp){
   var cels = kelvinToCel(temp);
   return (cels*(1.8)) + 32;
@@ -10,21 +9,16 @@ function kelvinToFahrenheit(temp){
 $(document).ready(function(){
   $('#toggleButton').click(function(){
     var tempString = $('#kelvin').text();
-    // console.log(tempString);
     var tempInt = parseInt(tempString);
-    // console.log(tempInt);
     $('#Celsius').text(kelvinToCel(tempInt));
     $('#Fahrenheit').text(kelvinToFahrenheit(tempInt));
     if($('#Fahrenheit').css('display') == 'none'){
       $('#Celsius').fadeToggle(1000,"swing");
       $('#Fahrenheit').delay(1000).fadeToggle(1000,"swing");
-
     }
     if($('#Celsius').css('display')=='none'){
       $('#Fahrenheit').fadeToggle(1000,"swing");
       $('#Celsius').delay(1000).fadeToggle(1000,"swing");
     }
-
-
   });
 });
